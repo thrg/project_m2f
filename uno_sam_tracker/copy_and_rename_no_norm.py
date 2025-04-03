a6f08f0ffc4e9a889d0e0bac3dde1ed072c1752a
chenga <cenga.marko@gmail.com>
2025-04-03 21:05:22 +0200

    rename no norm bug fix

--- uno_sam_tracker/copy_and_rename_no_norm.py
+++ uno_sam_tracker/copy_and_rename_no_norm.py
@@ -9,7 +9,7 @@ def copy_and_rename_files_in_subdir(input_folder):
             old_file_path = os.path.join(root, file)
 
             new_file_name = file.replace("_no_norm", "")
-            new_file_path = os.path.join(input_folder, new_file_name)
+            new_file_path = os.path.join(root, new_file_name)
 
             os.rename(old_file_path, new_file_path)
             print(f"Copied and renamed: {old_file_path} -> {new_file_path}")
